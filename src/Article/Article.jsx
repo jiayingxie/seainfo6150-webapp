df78d899b5a0f512c1a0870989a35bc3835525f4
Jiaying Xie <xie.jiay@northeastern.edu>
2021-02-20 14:05:20 +0800

    2nd version of html assignment

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -3,34 +3,33 @@ import React from 'react'
 const Article = () => {
   return (
     <div>
-      <section>
-        <h1>
-          The Statue of Liberty's torch heads to new museum
-        </h1>
+      <h1>
+        The Statue of Liberty's torch heads to new museum
+      </h1>
 
-        <address>
-          by Kate Farley (<a href="mailto:kate.farley@nytimes.com"> kate.farley@nytimes.com</a>)
+      <address>
+        by Kate Farley (<a href="mailto:kate.farley@nytimes.com"> kate.farley@nytimes.com</a>)
         </address>
-        <time datetime="2018-11-22">November 22, 2018</time> <br />
+      <time datetime="2018-11-22">November 22, 2018</time> <br />
 
-        <p>
-          The Statue of Liberty's original torch is getting a new home.
+      <p>
+        The Statue of Liberty's original torch is getting a new home.
         </p>
-        <p>
-          Made of copper and gold so that it would shine bright, the first torch took a beating from the weather and was replaced by a replica in  <time datetime="1984">1984</time>. It's been stored in the statue's pedestal ever since.
+      <p>
+        Made of copper and gold so that it would shine bright, the first torch took a beating from the weather and was replaced by a replica in  <time datetime="1984">1984</time>. It's been stored in the statue's pedestal ever since.
         </p>
-        <p>
-          But visitors will be able to see the original 3,600-pound torch in all its glory at a new Statue of Liberty Museum, which will open on Liberty Island in <time datetime="2019-05">May 2019</time>. The torch was transported there by truck last week, along with a replica model of Lady Liberty's face.
+      <p>
+        But visitors will be able to see the original 3,600-pound torch in all its glory at a new Statue of Liberty Museum, which will open on Liberty Island in <time datetime="2019-05">May 2019</time>. The torch was transported there by truck last week, along with a replica model of Lady Liberty's face.
         </p>
-        <p>
-          "Although it is not one of the most difficult things we have ever moved," says Douglas Phelps, who oversaw the relocation of the torch, "it is certainly the most important."
-        </p>
-      </section>
+      <p>
+        <q>Although it is not one of the most difficult things we have ever moved,</q>says Douglas Phelps, who oversaw the relocation of the torch, <q>it is certainly the most important.</q>
+      </p>
+
 
       <section>
-        <h1>
+        <h2>
           The history of the Statue of Liberty
-        </h1>
+        </h2>
         <p>
           Not only is the Statue of Liberty the symbol of New York City, she is also a <abbr title="United Nations Educational, Scientific and Cultural Organization">UNESCO</abbr> World Heritage site under the designation of <em>Outstanding Universal Value</em>.
         </p>
@@ -44,7 +43,7 @@ const Article = () => {
           Since then, the statue has served as a symbol of hope and possibility for many people who entered the United States as immigrants via neighboring Ellis Island.
         </p>
         <p>
-          "The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit inspired the decision to inscribe these famous lines on the statue's pedestal:
+          <q>The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy,</q>says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit inspired the decision to inscribe these famous lines on the statue's pedestal:
         </p>
         <blockquote>
           Give me your tired, your poor
@@ -63,9 +62,9 @@ const Article = () => {
       </section>
 
       <section>
-        <h1>
+        <h2>
           Some facts about the Statue of Liberty
-        </h1>
+        </h2>
 
         <ul>
           <li>
